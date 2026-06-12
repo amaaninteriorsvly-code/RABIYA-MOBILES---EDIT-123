f02a3756fcd96ceeb41ceab3b933e628b88ce2e9
amaaninteriorsvly-code <amaaninteriorsvly@gmail.com>
2026-06-12 16:22:52 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-view-1",
+      "name": "VIEW 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12897552912314758,
+          "pitch": 0.49120982654206813,
+          "rotation": 0,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-view-2",
+      "name": "VIEW 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05201887645892889,
+          "pitch": 0.6100788895768474,
+          "rotation": 0,
+          "target": "0-view-1"
+        },
+        {
+          "yaw": 1.5980836146147466,
+          "pitch": 0.7358227600816711,
+          "rotation": 0,
+          "target": "2-view-3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-view-3-",
+      "name": "VIEW 3 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7756889583143778,
+          "pitch": 0.9926389356713514,
+          "rotation": 0,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "RABIYA MOBILE SHOPE -  EDIT",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
